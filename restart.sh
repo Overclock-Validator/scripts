d8d1cbafcfdd143bf77bd0490e86002bbc0a1387
Overclock Validator <116830051+overclock-validator@users.noreply.github.com>
2022-10-27 23:27:47 -0500

    Create restart.sh

---
+++ restart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 /mnt/snapcheck.py
+sudo systemctl stop sol.service
+sudo pkill -9 -f solana-sys-tuner
+sudo $(command -v solana-sys-tuner) --user $(whoami) > sys-tuner.log 2>&1 &
+> /mnt/logs/solana-validator.log
+sudo systemctl start sol.service
